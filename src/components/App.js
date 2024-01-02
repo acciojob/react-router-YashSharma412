3af6ec8087a3ac64fd612623cef25a065f124790
YashSharma412 <sharmayashsatiish@gmail.com>
2024-01-02 11:00:15 +0530

    implemented Route feature.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import { Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
+import Home from "./Home";
+import About from "./About";
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
